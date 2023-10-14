1b8793e6b9b4f0a16e7ff3197b63f6bddc618bcf
buenhygoon <chochyjj@gmail.com>
2023-10-15 00:30:15 +0900

    theme 변경중

--- js/preferred-lang.js
+++
@@ -1,1 +0,0 @@
-(()=>{var n={fallback:"https://buenhyden.github.io/",homes:{en:"https://buenhyden.github.io/"}};(()=>{let o=navigator.language||navigator.userLanguage;if(o in n.homes){window.location.href=n.homes[o];return}let a=o.split("-");for(let e in n.homes)if(e.indexOf(a[0])===0){window.location.href=n.homes[e];return}window.location.href=n.fallback})();})();
